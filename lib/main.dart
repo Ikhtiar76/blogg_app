71c741bf6810dd5efad0345ded886581e9ac3cd7
Md Ikhtiar Alam <mdikhtiaralam@gmail.com>
2026-01-11 21:54:24 +0600

    Refactor dependency injection and initialize Supabase in init_dependencies.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,19 @@
-import 'package:blogg_app/core/secrets/app_secrets.dart';
 import 'package:blogg_app/core/theme/theme.dart';
-import 'package:blogg_app/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:blogg_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blogg_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blogg_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blogg_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blogg_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 // supabase database pass: 76-PrantoBlogg
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseurl,
-    anonKey: AppSecrets.supabaseAnonkey,
-  );
+  await initDependencies();
+
   runApp(
     MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => AuthBloc(
-            userSignUp: UserSignUp(
-              authRepository: AuthRepositoryImpl(
-                authRemoteDataSource: AuthRemoteDataSourceImpl(
-                  supabaseClient: supabase.client,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
+      providers: [BlocProvider(create: (_) => serviceLocator<AuthBloc>())],
       child: const MyApp(),
     ),
   );
