b220ea3b4fb6dd01cf29cfd18e4697d4f87c3125
Md Ikhtiar Alam <mdikhtiaralam@gmail.com>
2026-01-11 17:00:47 +0600

    Implement authentication feature with sign-up functionality and BLoC pattern

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,36 @@
+import 'package:blogg_app/core/error/exceptions.dart';
+import 'package:blogg_app/core/error/failures.dart';
+import 'package:blogg_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blogg_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+  AuthRepositoryImpl({required this.authRemoteDataSource});
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithNameEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await authRemoteDataSource.signUpWithNameEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(message: e.message));
+    }
+  }
+}
---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,29 @@
+import 'package:blogg_app/core/error/failures.dart';
+import 'package:blogg_app/core/usecase/usecase.dart';
+import 'package:blogg_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  UserSignUp({required this.authRepository});
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithNameEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
