c04a2696db18813ab3c228a92ef759af15f44985
Md Ikhtiar Alam <mdikhtiaralam@gmail.com>
2026-01-13 13:51:23 +0600

    Add BlogModel class for blog data representation and JSON serialization

---
+++ lib/features/blog/data/models/blog_model.dart
@@ -0,0 +1,37 @@
+import 'package:blogg_app/features/blog/domain/entities/blog.dart';
+
+class BlogModel extends Blog {
+  BlogModel({
+    required super.id,
+    required super.posterId,
+    required super.title,
+    required super.content,
+    required super.imageUrl,
+    required super.topics,
+    required super.updatedAt,
+  });
+  factory BlogModel.fromJson(Map<String, dynamic> json) {
+    return BlogModel(
+      id: json['id'],
+      posterId: json['poster_id'],
+      title: json['title'],
+      content: json['content'],
+      imageUrl: json['image_url'],
+      topics: List<String>.from(json['topics'] ?? []),
+      updatedAt: json['updated_at'] == null
+          ? DateTime.now()
+          : DateTime.parse(json['updated_at']),
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'id': id,
+      'poster_id': posterId,
+      'title': title,
+      'content': content,
+      'image_url': imageUrl,
+      'topics': topics,
+      'updated_at': updatedAt.toIso8601String(),
+    };
+  }
+}
